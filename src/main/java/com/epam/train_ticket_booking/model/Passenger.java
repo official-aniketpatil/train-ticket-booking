97affcc0f50bfa64e59ef961e54efd563a203b22
Aniket Patil <official.aniketpatil@gmail.com>
2019-08-12 09:12:39 +0530

    init

---
+++ src/main/java/com/epam/train_ticket_booking/model/Passenger.java
@@ -0,0 +1,69 @@
+package com.epam.train_ticket_booking.model;
+
+public class Passenger {
+	private long id;
+	private String name;
+	private String gender;
+	private String mobile;
+	
+	public Passenger(long id, String name, String gender, String mobile) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.mobile = mobile;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public String getMobile() {
+		return mobile;
+	}
+	
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	@Override
+	public String toString() {
+		return "Passenger [id=" + id + ", name=" + name + ", gender=" + gender + ", mobile=" + mobile + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return (int)this.getId();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		
+		Passenger other = (Passenger) obj;
+		return other.getId() == this.getId();
+	}
+}
+
---
+++ src/main/java/com/epam/train_ticket_booking/model/Train.java
@@ -0,0 +1,53 @@
+package com.epam.train_ticket_booking.model;
+
+public class Train {
+	private long id;
+	private String source;
+	private String destination;
+	private double fare;
+	
+	public Train(long id, String source, String destination, double fare) {
+		super();
+		this.id = id;
+		this.source = source;
+		this.destination = destination;
+		this.fare = fare;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public double getFare() {
+		return fare;
+	}
+
+	public void setFare(double fare) {
+		this.fare = fare;
+	}
+
+	@Override
+	public String toString() {
+		return "Train [id=" + id + ", source=" + source + ", destination=" + destination + ", fare=" + fare + "]";
+	}
+}
